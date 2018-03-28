7dc64f01cf4586cddd526d013c1174d1d4a1a36e
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-03-27 19:22:05 -0700

    1.WarmUp 2.CrazyCatLady

---
+++ src/WarmUp.java
@@ -0,0 +1,29 @@
+import javax.swing.JOptionPane;
+
+public class WarmUp {
+public static void main(String[] args) {
+	
+	String Word= JOptionPane.showInputDialog("Type a word Down Below");
+	
+	String Number=JOptionPane.showInputDialog("Type a number");
+	int Num=Integer.parseInt(Number);
+	
+	for (int i = Num; i<Num; i++) {
+		
+		System.out.println(Word);
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
