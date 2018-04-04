5fbd9b6e881ea262ae4da21b65e66f3945287ac1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-03 19:24:00 -0700

    1. HighLowGame

--- src/elseif/HighLowGame.java
+++ src/elseif/HighLowGame.java
@@ -5,32 +5,54 @@ package elseif;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+	Random random=new Random();
+		int Number= random.nextInt(100)+1;
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(Number);
+	
 		// 11. Repeat steps 1 to 10 ten times
+		for (int i= 0; i < 10; i++) {
+			
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
-			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+String Guess=JOptionPane.showInputDialog("Guess a number between 1-100");
 			
-			// 5. if the guess is correct
-				// 6. Win
-				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
-			// 7. if the guess is high
-				// 8. Tell them it's too high
-			// 9. if the guess is low
-				// 10. Tell them it's too low
-
-		// 13. Tell them they lose
+// 4. Convert the users’ answer to an int (Integer.parseInt(string))
+			int a = Integer.parseInt(Guess);
 		
-	}
+			// 5. if the guess is correct
+			if(a==Number){
+		
+				// 6. Win
+				JOptionPane.showMessageDialog(null, "YOU WIN");
+				System.exit(0);
+			}
+			
+				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
+		
+		// 7. if the guess is high
+			else if(a<Number) {
+				JOptionPane.showMessageDialog(null,"Your number is too high");
+			}
+				// 8. Tell them it's too high
+				// 9. if the guess is low
+			else if(a>Number) {
+				JOptionPane.showMessageDialog(null,"Your number is too Low");	
+			}
+				
+			// 10. Tell them it's too low
+					// 13. Tell them they lose
 
+		}	
+		JOptionPane.showMessageDialog(null," You LOOSE");
+	}
 }
 
 
