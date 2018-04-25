32b37ff034cc7d5c62e64f6299fe4573f0d6f662
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-04-24 19:21:34 -0700

    1. NinetyNineBottles 2.RobotColorChooser

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -4,24 +4,46 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
-		
+	Robot robot=new Robot();	
+	for(;;) {	
 		//3. Ask the user what color they would like the robot to draw
+			String color=JOptionPane.showInputDialog("What color would you like the robot to draw?");
 		
 		//5. Use an if/else statement to set the pen color that the user requested
-
+		if(color.equals("Red")) {
+			robot.setPenColor(Color.RED);
+		}
         //6. If the user doesn’t enter anything, choose a random color
-
+		else if(color.equals("Blue")) {
+			robot.setPenColor(Color.BLUE);
+		}
+		else {
+			robot.setRandomPenColor();
+		}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 		
-		//4. Set the pen width to 10
 		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
+		//4. Set the pen width to 10
+		robot.setPenWidth(7);
+	   
+		//2. Make the robot draw a shape (this will take more than one line of code)
+	robot.penDown();
+	robot.move(100);
+	robot.turn(90);
+	robot.move(100);
+	robot.turn(90);
+	robot.move(100);
+	robot.turn(90);
+	robot.move(90);
 	}
+	}
+	
 }
