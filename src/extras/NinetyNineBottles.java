9993803dc7b0928cc67ad054ecdb98bf59a44c90
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-05-01 19:19:53 -0700

    1. NinetyNineBottles

--- src/extras/NinetyNineBottles.java
+++ src/extras/NinetyNineBottles.java
@@ -1,5 +1,22 @@
 package extras;
 
 public class NinetyNineBottles {
+	public static void main(String[] args) {
+		
+	String string1="Bottles of beer";
+	String string2=" on the wall";
+	String string3="Take one down,pass it around";
+		
+	for (int i = 99; i > 0; i--) {
+	
+		System.out.println(i);
+		System.out.println(i+string1+string2+string3);
+	
+	
+	}
+		
+		
+	}
 
-}
+} 
+
